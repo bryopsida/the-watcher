07270a6e32281b6de109085ee480f8d355a35315
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-07-08 08:54:09 -0500

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,11 @@ import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { HealthModule } from './health/health.module'
 import { LoggerModule } from 'nestjs-pino'
+import { KubernetesModule } from './kubernetes/kubernetes.module';
+import { NotificationModule } from './notification/notification.module';
+import { ScannerModule } from './scanner/scanner.module';
 @Module({
-  imports: [ConfigModule.forRoot(), LoggerModule.forRoot(), HealthModule],
+  imports: [ConfigModule.forRoot(), LoggerModule.forRoot(), HealthModule, KubernetesModule, NotificationModule, ScannerModule],
   controllers: [AppController],
   providers: [AppService],
 })
